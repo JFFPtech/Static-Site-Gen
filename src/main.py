556f41ddb95785e148135c86302ae9dddb5c00ef
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-31 07:38:37 -0700

    test for main.sh prints

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode
+
+def main():
+    # Create a new TextNode object with dummy values
+    text_node = TextNode("Title", "This is some text")
+
+    # Print the object
+    print(text_node)
+
+# Call the main function
+main()
